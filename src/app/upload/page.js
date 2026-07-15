6ff90e566fa69e99b41fb5efececdfa8f2f90dad
DVSR1411 <satwikreddy.danda@hotmail.com>
2026-07-15 14:19:32 +0530

    Bug fixes

--- src/app/upload/page.js
+++ src/app/upload/page.js
@@ -1,6 +1,6 @@
 import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
-import UploadPage from './page.jsx';
+import UploadPage from './UploadClient.jsx';
 
 export default async function UploadServerPage() {
   const session = await auth();
